54d4397d142d98b2572f3f2c5ccc1f5b369f7a3e
Taekyung Kim <gnuykeat.mik@gmail.com>
2026-05-29 00:46:25 +0900

    nightfox
    Signed-off-by: Taekyung Kim <gnuykeat.mik@gmail.com>

--- init.lua
+++ init.lua
@@ -305,10 +305,15 @@ do
     },
   }
 
+  vim.pack.add { gh 'EdenEast/nightfox.nvim' }
+  require('nightfox').setup {
+
+  }
+
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'nightfox'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
