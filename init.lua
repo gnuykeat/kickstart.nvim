09c6ec326067d25460c6b35d97c2352f31795a7a
Taekyung Kim <gnuykeat.mik@gmail.com>
2026-06-07 13:26:21 +0900

    localleader should be ,
    Signed-off-by: Taekyung Kim <gnuykeat.mik@gmail.com>

--- init.lua
+++ init.lua
@@ -10,7 +10,7 @@ do
   -- See `:help mapleader`
   --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
   vim.g.mapleader = ' '
-  vim.g.maplocalleader = ' '
+  vim.g.maplocalleader = ','
 
   -- Set to true if you have a Nerd Font installed and selected in the terminal
   vim.g.have_nerd_font = true
@@ -85,6 +85,11 @@ do
   -- instead raise a dialog asking if you wish to save the current file(s)
   -- See `:help 'confirm'`
   vim.o.confirm = true
+
+  -- [[ Tab Behavior ]]
+  vim.o.tabstop = 2
+  vim.o.shiftwidth = 2
+  vim.o.softtabstop = 2
 
   -- [[ Basic Keymaps ]]
   --  See `:help vim.keymap.set()`
